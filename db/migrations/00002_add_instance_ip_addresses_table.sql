f8792d9802df658915c9f5c0d7e1a8cc6795f9fb
Jordan Day <jordan@roboninja.com>
2022-06-07 15:34:09 -0500

    fix uppercase in column name

--- db/migrations/00002_add_instance_ip_addresses_table.sql
+++ db/migrations/00002_add_instance_ip_addresses_table.sql
@@ -3,7 +3,7 @@
 
 CREATE TABLE instance_ip_addresses (
   id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
-  instance_ID UUID NOT NULL,
+  instance_id UUID NOT NULL,
   address INET NOT NULL,
   created_at TIMESTAMPTZ NOT NULL,
   updated_at TIMESTAMPTZ NOT NULL
