0de3cdad40b80ede4f2be3556a65c53660fc6912
Jordan Day <jordan@roboninja.com>
2022-06-13 13:46:56 -0500

    Add middleware for identifying requesting instance by IP address

---
+++ internal/middleware/identify_instance.go
@@ -0,0 +1,69 @@
+package middleware
+
+import (
+	"database/sql"
+	"errors"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+
+	"go.hollow.sh/metadataservice/internal/models"
+)
+
+// ContextKeyInstanceID is the magic string set in the gin.Context key/value
+// store used for storing the ID of the instance making the request, if the
+// instance has been identified.
+const ContextKeyInstanceID = "instance-id"
+
+// When a request comes in to the /metadata or /userdata endpoints (or the 2009-04-04/* variants)
+// we need to identify the instance making the request.
+// There's 2 ways to do this:
+// a) (pending) if the request was made with the special auth header that tells
+// us the request is being proxied for the instance through another system
+// (like a switch), use the auth header info to get the instance ID.
+// OR
+// b) via the request ip from the instance making the request.
+//
+// For case (a), we'll know the instance ID, and can check if we have metadata
+// or userdata stored for that ID. If not, we need to fetch it from an external
+// system.
+// For case (b), we'll look up the instance ID from our instance_ip_addresses
+// table. If there's no rows matching the request IP, we'll know we need to
+// fetch it from an external system.
+
+// IdentifyInstanceByIP is used to determine the ID of the instance making the
+// request by looking at the request IP.
+// If a row in the instance_ip_addresses table is found with a matching IP
+// address, we set the instance ID in the context.
+func IdentifyInstanceByIP(db *sqlx.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var (
+			address           string
+			instanceIPAddress *models.InstanceIPAddress
+			err               error
+		)
+
+		// TODO: Add support for configuring trusted proxies.
+		// When trusted proxies are configured in gin, ClientIP() will use the
+		// X-Forwarded-For or X-Real-Ip headers (if present) to report the remote
+		// IP. If trusted proxies are not configured, these headers will be ignored
+		// to prevent spoofing by clients, and instead the request's RemoteAddr
+		// will be returned.
+		// But if a proxy is sitting in front of this service, RemoteAddr will be
+		// the IP of the proxy, and not the requestor.
+		address = c.ClientIP()
+
+		instanceIPAddress, err = models.InstanceIPAddresses(qm.Where("address >>= ?::inet", address)).One(c, db)
+		if err != nil && !errors.Is(err, sql.ErrNoRows) {
+			// Internal server error, presumably
+			// TODO: raise error with whatever is the right way to do that for a gin middleware
+			return
+		}
+
+		if instanceIPAddress != nil {
+			// We found the row, set the instnace ID into the gin context.
+			c.Set(ContextKeyInstanceID, instanceIPAddress.InstanceID)
+		}
+	}
+}
